a4339a19a1fb6fcca6172d7fa9015e76d7113345
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-14 13:11:45 +0100

    React Essentials 4°

--- React Essentials/src/App.jsx
+++ React Essentials/src/App.jsx
@@ -1,3 +1,5 @@
+import reactIcon from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Foundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -9,7 +11,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactIcon} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
