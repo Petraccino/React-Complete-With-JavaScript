b958e357dc86b396d3615437dce3808aa5749619
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-18 12:31:27 +0100

    React Essential Deep Dive 5°

--- React Essential Deep Dive/src/components/player/Player.jsx
+++ React Essential Deep Dive/src/components/player/Player.jsx
@@ -1,12 +1,27 @@
 import "./Player.css";
+import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({ name, symbol }) {
+  const [selectedTopic, setSelectedTopic] = useState(false);
+
+  function handleEdit() {
+    setSelectedTopic((isEdit) => !isEdit);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {selectedTopic ? (
+          <input type="text" required value={name}/>
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        {selectedTopic ? (
+          <button onClick={handleEdit}>Save</button>
+        ) : (
+          <button onClick={handleEdit}>Edit</button>
+        )}
       </span>
     </li>
   );
