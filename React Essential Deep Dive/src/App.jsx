6153906eb441157f5286e2d0294c8e5616aa0c27
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-18 10:59:40 +0100

    React Essential Deep Dive 3°

--- React Essential Deep Dive/src/App.jsx
+++ React Essential Deep Dive/src/App.jsx
@@ -1,10 +1,26 @@
-
 function App() {
-  
-
   return (
-    <h1>Coming soon...</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+              <button>Edit</button>
+            </span>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+              <button>Edit</button>
+            </span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
