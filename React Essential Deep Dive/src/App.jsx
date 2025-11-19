e7ac1abe8f0a4ef9e0fb2581dfce981eca1c7171
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-19 16:10:31 +0100

    React Essential Deep Dive 12°

--- React Essential Deep Dive/src/App.jsx
+++ React Essential Deep Dive/src/App.jsx
@@ -2,24 +2,19 @@ import GameBoard from "./components/gameboard/GameBoard";
 import Log from "./components/log/Log";
 import Player from "./components/player/Player";
 import { useState } from "react";
+import { deriveActivePlayer } from "./util/Util";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurn] = useState([]);
 
-  function handleActivePlayer(rowIndex, colIndex) {
-    setActivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
-    );
-    setGameTurn((prevTurn) => {
-      let currentPlayer = "X";
+  const activePlayer = deriveActivePlayer(gameTurns);
 
-      if (prevTurn.length > 0 && prevTurn[0].player === "X") {
-        currentPlayer = "O";
-      }
+  function handleActivePlayer(rowIndex, colIndex) {
+    setGameTurn((prevTurn) => {
+      const activePlayer = deriveActivePlayer(prevTurn);
 
       const updateTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
         ...prevTurn,
       ];
 
@@ -31,12 +26,20 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive ={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive ={activePlayer === "O"}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
         <GameBoard onSelectSquare={handleActivePlayer} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
