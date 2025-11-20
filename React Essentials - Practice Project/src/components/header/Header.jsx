f9b54d1a1776eb6f4162dcb8685b039c98fb580f
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-20 16:11:24 +0100

    Introduzione best practice Header component

--- React Essentials - Practice Project/src/components/header/Header.jsx
+++ React Essentials - Practice Project/src/components/header/Header.jsx
@@ -2,9 +2,9 @@ import "./Header.css";
 import img from "../../assets/investment-calculator-logo.png"
 export default function Header() {
   return (
-    <div id="header">
-      <img src={img} />
+    <header  id="header">
+      <img src={img} alt="Investment calculator logo" />
       <h1>React Investment Calculator</h1>
-    </div>
+    </header>
   );
 }
