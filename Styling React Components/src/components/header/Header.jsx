6b77a1220f933bfa83bd41261c087a73528af0e0
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-21 17:17:58 +0100

    Styling React Components 3°

--- Styling React Components/src/components/header/Header.jsx
+++ Styling React Components/src/components/header/Header.jsx
@@ -1,19 +1,17 @@
 import logo from "../../assets/logo.png";
 import "./Header.css";
 
+const styleInline = {
+  color: "red",
+  textAlign: "left",
+};
+
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p
-        style={{
-          color: "red",
-          textAlign: "left",
-        }}
-      >
-        A community of artists and art-lovers.
-      </p>
+      <p style={styleInline}>A community of artists and art-lovers.</p>
     </header>
   );
 }
