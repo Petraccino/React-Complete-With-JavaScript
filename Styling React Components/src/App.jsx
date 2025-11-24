0da419c321f5d98ba721cfa85b1c36c44cbdcf56
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-24 12:59:09 +0100

    Configuration tailwindcss

--- Styling React Components/src/App.jsx
+++ Styling React Components/src/App.jsx
@@ -1,5 +1,6 @@
 import AuthInputs from './components/authinput/AuthInputs.jsx';
 import Header from './components/header/Header.jsx';
+import './index.css';
 
 export default function App() {
   return (
