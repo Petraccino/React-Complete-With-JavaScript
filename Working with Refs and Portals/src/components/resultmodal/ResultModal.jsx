938a40dee905820ef60af182c9302a952d760f22
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-27 10:58:53 +0100

    Working with Refs and Portals 8°

---
+++ Working with Refs and Portals/src/components/resultmodal/ResultModal.jsx
@@ -0,0 +1,18 @@
+import "./ResultModal.css";
+
+export default function ResultModal({ result, targetTime }) {
+  return (
+    <dialog className="result-modal" open>
+      <h2>You {result}</h2>
+      <p>
+        The target time wae <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
