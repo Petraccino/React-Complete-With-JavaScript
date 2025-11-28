15e8f10c014fe80571e31b1d0b31f942e04d53e5
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-28 10:24:37 +0100

    Practice Project Project Management App with Components, State, Refs and More 5°

--- Practice Project Project Management App with Components, State, Refs and More/src/App.jsx
+++ Practice Project Project Management App with Components, State, Refs and More/src/App.jsx
@@ -1,12 +1,35 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevProjectsState) => {
+      return {
+        ...prevProjectsState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
