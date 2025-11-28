9ac224f7a5d17a17f7ba7c9cd195b0d00d5c2e62
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-11-28 16:07:18 +0100

    Practice Project Project Management App with Components, State, Refs and More 13°

--- Practice Project Project Management App with Components, State, Refs and More/src/components/ProjectDetails.jsx
+++ Practice Project Project Management App with Components, State, Refs and More/src/components/ProjectDetails.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 export default function ProjectDetails({ project, onDelete }) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
@@ -23,7 +25,7 @@ export default function ProjectDetails({ project, onDelete }) {
           {project.description}
         </p>
       </header>
-      TASKS
+      <Tasks/>
     </div>
   );
 }
---
+++ Practice Project Project Management App with Components, State, Refs and More/src/components/Task.jsx
@@ -0,0 +1,6 @@
+export default function Task() {
+    return <div className="flex items-center justify-between">
+        <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" type="text" />
+        <button className="text-stone-600 hover:text-stone-950">Add Task</button>
+    </div>
+} 
