852c430ea2ab835d8f3ca6677dde3522c891a4ab
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-12-09 14:46:08 +0100

    Side Effects useEffect 6°

--- Side Effects useEffect/src/components/Modal.jsx
+++ Side Effects useEffect/src/components/Modal.jsx
@@ -1,11 +1,18 @@
-import { useImperativeHandle, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
 export default function ({ children, open }) {
   const dialog = useRef();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
 
   return createPortal(
-    <dialog className="modal" open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById("modal")
