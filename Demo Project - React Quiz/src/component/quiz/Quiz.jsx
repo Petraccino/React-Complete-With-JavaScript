388f0e656da7b6ae5aedcd07f6edfce7a07466d5
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-12-09 19:51:35 +0100

    Demo Project - React Quiz 3°

--- Demo Project - React Quiz/src/component/quiz/Quiz.jsx
+++ Demo Project - React Quiz/src/component/quiz/Quiz.jsx
@@ -1,4 +1,5 @@
 import "./Quiz.css";
+import imgQuizComplete from "../../assets/quiz-complete.png";
 import { useState } from "react";
 import QUESTIONS from "../../question";
 
@@ -6,6 +7,7 @@ export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestion = userAnswers.length;
+  const quizIsComplete = QUESTIONS.length === activeQuestion;
 
   function handleSelectAnswer(answer) {
     setUserAnswers((prevUserAnswers) => {
@@ -13,14 +15,24 @@ export default function Quiz() {
     });
   }
 
-  console.log(userAnswers);
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={imgQuizComplete} alt="Quiz Logo complete" />
+        <h2>Quix Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffleAnswers = [...QUESTIONS[activeQuestion].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestion].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestion].answers.map((answer) => {
+          {shuffleAnswers.map((answer) => {
             return (
               <li key={answer} className="answer">
                 <button onClick={() => handleSelectAnswer(answer)}>
